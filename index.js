2105c49b695eeede86c8327d976814729126048a
Jason Adindu <jasonadd47@gmail.com>
2023-02-27 18:35:07 -0600

    Create index

--- index.js
+++ index.js
@@ -9,6 +9,3 @@ if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
 console.log(`${year} is ${isLeapYear ? 'a leap year' : 'not a leap year'}`);
 console.log();
 
-
-const is_Leapyear = year => new Date(year, 1, 29).getMonth() === 1;
-console.log(is_Leapyear());
